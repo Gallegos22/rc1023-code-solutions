bc2aae0dd50a0f55da8871b455fa64841faae874
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2023-10-24 01:36:44 +0000

    Better formatted javascript-arrays Exercise

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -1,4 +1,5 @@
 const colors = ['red', 'white', 'blue'];
+
 console.log('value of colors[0]:', colors[0]);
 console.log('value of colors[1]:', colors[1]);
 console.log('value of colors[2]:', colors[2]);
@@ -28,6 +29,7 @@ console.log(
 console.log('value of colors:', colors);
 
 const students = ['Leo', 'Ray', 'Sean', 'Khoa'];
+
 const numberOfStudents = students.length;
 console.log('There are ' + numberOfStudents + ' ' + 'students in the class.');
 const lastIndex = numberOfStudents - 1;
